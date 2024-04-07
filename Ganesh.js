0c0c5702d49f2f36c68f3e9db54cdd1e5f293b65
ganeshdeulkar1196 <82272284+ganeshdeulkar1196@users.noreply.github.com>
2024-04-07 21:35:38 +0530

    Update Ganesh.js

--- Ganesh.js
+++ Ganesh.js
@@ -1,7 +1,1 @@
-const name = 'Ganesh';
-const age = 22;
 
-// Using template literals
-console.log(My name is ${name} and I am ${age} years old.);
-
-// Output: My name is Alice and I am 25 years old.
